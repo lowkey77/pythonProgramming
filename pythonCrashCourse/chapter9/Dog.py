1267a0d6a9d3840e960b760bedc35e75a5120568
Reggie Key <rkey02@harris.com>
2019-10-15 13:54:57 -0600

    Working on chapter 9

---
+++ pythonCrashCourse/chapter9/Dog.py
@@ -0,0 +1,23 @@
+# Dog class
+class Dog:
+    """A simple attempt to model a dog"""
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting>")
+
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command"""
+        print(f"{self.name} rolled over!")
+
+
+myDog = Dog("Willie", 6)
+
+print(f"My dog's name is {myDog.name}")
+print(f"My dog is {myDog.age} years old.")
