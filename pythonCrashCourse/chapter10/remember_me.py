254ee18f8c780e32e4f3e3d6faac43dd4d99901f
reggie <lowkey77@gmail.com>
2019-10-29 22:57:42 -0600

    Updated after refactor to greet_user method

--- pythonCrashCourse/chapter10/remember_me.py
+++ pythonCrashCourse/chapter10/remember_me.py
@@ -1,14 +1,19 @@
 import json
 
-filename = "username.json"
 
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
-    username = input("What is your name ")
-    with open(filename, "w") as f:
-        json.dump(str(username), f)
-        print(f"We'll remember you when you come back, {username.title()}!")
-else:
-    print(f"Welcome back {username}!")
+def greet_user():
+    """Greet the user by name."""
+    filename = "username.json"
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("What is your name ")
+        with open(filename, "w") as f:
+            json.dump(str(username), f)
+            print(f"We'll remember you when you come back, {username.title()}!")
+    else:
+        print(f"Welcome back {username}!")
+
+
+greet_user()
