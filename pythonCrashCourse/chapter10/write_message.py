ed0837a9487e9dcaba7d7b39282901207c4e2ac0
reggie <lowkey77@gmail.com>
2019-10-29 21:01:55 -0600

    More chapter 10 practice doing file writing and exception handling (try/except).

--- pythonCrashCourse/chapter10/write_message.py
+++ pythonCrashCourse/chapter10/write_message.py
@@ -1,5 +1,10 @@
+# Writing and appending to a file
 filename = "programming.txt"
 
-with open(filename, "w") as file_object:
-    file_object.write("I love programming.\n")
-    file_object.write("I love creating new games.\n")
+# with open(filename, "w") as file_object:
+#     file_object.write("I love programming.\n")
+#     file_object.write("I love creating new games.\n")
+
+with open(filename, "a") as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
