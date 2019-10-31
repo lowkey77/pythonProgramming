3f7de29efdbee18c4899c290d6303eeff96c3d7b
Reggie Key <lowkey77@gmail.com>
2019-10-31 09:12:02 -0600

    Working on writing tests for Python code.

--- pythonCrashCourse/chapter10/remember_me.py
+++ pythonCrashCourse/chapter10/remember_me.py
@@ -1,19 +1,34 @@
 import json
 
 
-def greet_user():
-    """Greet the user by name."""
+def get_stored_username():
+    """Get stored username if available."""
     filename = "username.json"
     try:
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
-        username = input("What is your name ")
-        with open(filename, "w") as f:
-            json.dump(str(username), f)
-            print(f"We'll remember you when you come back, {username.title()}!")
+        return None
     else:
-        print(f"Welcome back {username}!")
+        return str(username)
+
+
+def get_new_username():
+    username = input("What is your name? ")
+    filename = "username.json"
+    with open(filename, "w") as f:
+        json.dump(username, f)
+    return username
+
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username.title()}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username.title()}!")
 
 
 greet_user()
---
+++ pythonCrashCourse/chapter11/test/test_name_function.py
@@ -0,0 +1,20 @@
+import unittest
+from name_function import get_formatted_name
+
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+
+    def test_first_last_name(self):
+        """Do names like 'John Doe' work?"""
+        formatted_name = get_formatted_name("john", "doe")
+        self.assertEqual(formatted_name, "John Doe")
+
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name("wolfgang", "mozart", "amadeus")
+        self.assertEqual(formatted_name, "Wolfgang Amadeus Mozart")
+
+
+if __name__ == "__main__":
+    unittest.main()
